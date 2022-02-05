fea00288230159cafe13ef402d8f51d2c2aa8025
dokodesara <95309661+dokodesara@users.noreply.github.com>
2022-02-05 13:43:10 -0500

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-//display current time
-function showTime() {
-    dateEl.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $("#time").textContent(TimeEl);
-    console.log(TimeEl);
-}
-setInterval(showTime, 1000);localStorage
